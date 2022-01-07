5e5d878c17682afd9ca89d23a505848c7ef1bc9a
poliana <poliana.karenn05@gmail.com>
2022-01-07 17:18:15 -0300

    transações

--- scripts.js
+++ scripts.js
@@ -16,3 +16,46 @@ const Modal = {
             .remove('active')
     }
 }
+
+const transaction = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    },
+]
+
+//Organizando as ideias
+//eu preciso somas as entradas
+//depois preciso somar as saidas
+// remover das entradas o valor das saidas
+//assim, eu terei o total
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses(){
+        //somas as saidas
+    },
+    total() {
+        //entradas - saidas
+    }
+}
+
+//proximo passo
+//Agora eu preciso substituir os dados do html com os dados do JS
+
